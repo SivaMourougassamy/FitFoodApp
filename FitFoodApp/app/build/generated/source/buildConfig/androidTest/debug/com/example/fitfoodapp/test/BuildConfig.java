5ba91785970bf16848d84073b98cbf412d1f295c
SivaMourougassamy <56765191+SivaMourougassamy@users.noreply.github.com>
2019-10-19 13:10:41 -0400

    Add files via upload

---
+++ FitFoodApp/app/build/generated/source/buildConfig/androidTest/debug/com/example/fitfoodapp/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.fitfoodapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.fitfoodapp.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
